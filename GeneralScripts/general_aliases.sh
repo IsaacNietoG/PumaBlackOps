1c6d43ba0ef15f54ad241c88626610db6e9ecde6
mrtaichi <isaaniet@gmail.com>
2025-07-10 23:54:04 -0600

    feat: target command and general aliases

---
+++ GeneralScripts/general_aliases.sh
@@ -0,0 +1,6 @@
+# Aliases útiles para pentesting y manejo de targets
+# Fuente este archivo en tu ~/.bashrc o ~/.zshrc con:
+#   [ -f /ruta/a/GeneralScripts/target_aliases.sh ] && source /ruta/a/GeneralScripts/target_aliases.sh
+
+alias httpserver='python3 -m http.server' 
+alias listen='nc -lvnp'
